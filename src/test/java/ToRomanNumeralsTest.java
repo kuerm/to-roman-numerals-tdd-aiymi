14e934f1a4876e5a5568f8a19462a863ff89c815
Kuerm <kuerm@gmx.net>
2024-04-05 16:06:59 +0200

    29

--- src/test/java/ToRomanNumeralsTest.java
+++ src/test/java/ToRomanNumeralsTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ToRomanNumeralsTest {
     private static final Map<Integer, String> map = new HashMap<>() {{
+        put(1000, "M");
+        put(10, "X");
         put(5, "V");
         put(4, "IV");
         put(1, "I");
@@ -17,6 +21,21 @@ class ToRomanNumeralsTest {
         String expected = "VI";
         int input = 6;
 
+        String actual = toRoman(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, I",
+            "2, II",
+            "4, IV",
+            "5, V",
+            "10, X",
+            "3000, MMM"
+    })
+    void toRoman(int input, String expected) {
         String actual = toRoman(input);
 
         assertThat(actual).isEqualTo(expected);
