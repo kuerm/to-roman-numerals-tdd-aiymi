cd0d089b4a72410e040888fb0812fee9529a7711
Kuerm <kuerm@gmx.net>
2024-04-05 15:09:25 +0200

    0

--- src/test/java/ToRomanNumerals.java
+++ src/test/java/ToRomanNumerals.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ToRomanNumerals {
     @Test
-    void dada() {
-        assertThat(1).isEqualTo(1);
+    void one() {
+        assertThat("I").isEqualTo("I");
     }
 }
