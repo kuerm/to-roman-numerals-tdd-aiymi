34e862370e1a94592b7853f18a4db379121e389c
Kuerm <kuerm@gmx.net>
2024-04-05 14:40:51 +0200

    Initial

---
+++ src/test/java/ToRomanNumerals.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ToRomanNumerals {
+    @Test
+    void dada() {
+        assertThat(1).isEqualTo(1);
+    }
+}
